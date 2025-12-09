285bec2a5675aa22ce48cfd234117b5a89e7f22c
Mark Parker <markstjohnparker@gmail.com>
2025-12-09 18:37:46 +0000

    More unit tests

---
+++ Test/UnitTests.cs
@@ -0,0 +1,47 @@
+using SpiderProj;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void Test1_NormalTravel()
+        {
+            var wall = new Wall(7, 15);
+            var spider = new Spider(wall, 4, 10, Spider.Orientation.Left);
+            var instructions = "FLFLFRFFLF";
+
+            spider.Obey(instructions);
+
+            Assert.AreEqual("X: 5 Y: 7  Facing: Right", spider.Position);
+        }
+
+        [TestMethod]
+        public void Test2_OffWallStart()
+        {
+            var wall = new Wall(7, 15);
+
+            var ex = Assert.ThrowsException<ArgumentException>(() =>
+            {
+                var spider = new Spider(wall, 8, 10, Spider.Orientation.Left);
+            });
+
+            Assert.AreEqual("Requested spider start position is off the wall.", ex.Message);
+        }
+
+        [TestMethod]
+        public void Test3_OffWallMove()
+        {
+            var wall = new Wall(7, 15);
+            var spider = new Spider(wall, 2, 3, Spider.Orientation.Left);
+            var instructions = "FFFFF";
+
+            var ex = Assert.ThrowsException<ArgumentOutOfRangeException>(() => spider.Obey(instructions));
+
+            Assert.AreEqual(
+                "Specified argument was out of the range of valid values. (Parameter 'Cannot move further left.')",
+                ex.Message);
+        }
+    }
+}
